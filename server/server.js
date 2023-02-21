797d9955874abc4f79134cadd7a2c83cb8118609
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-21 18:50:03 -0300

    [server] Configuraciones iniciales

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const log = require("./components/displayLog");
+const express = require("express");
+const cors = require("cors");
+
+// * Inicia express
+const app = express();
+const port = 8000;
+
+app.listen(
+	port,
+	() => { log.infoLog(`Puerto habilitado: ${port}`)}
+);
+
+// * Se habilita MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
