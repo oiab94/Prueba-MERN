0d6ffd91f6833a6dab6c99d734731c55cdbba5ac
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-02-25 21:00:54 -0300

    [server] Indica errores que pueda ocurrir al crear User

--- server/controllers/user.new.controller.js
+++ server/controllers/user.new.controller.js
@@ -1,19 +1,37 @@
 const user = require("../models/user.model");
-const { hashPassword } = require("../components/hashAPassword");
 const log = require("../components/displayLog");
+
+// * Maneajador de errores
+const handleErrros = (err) => {
+	let errors = {email:"", password:""};
+
+	// * Validamos el error
+	if(err.message.includes("user validation failed"))
+	// * Quitamos los errores que debemos saber
+		Object.values(err.errors)
+			.forEach(
+				// * Indicamos a nuestro objeto el error indicado
+				({properties}) => {
+					errors[properties.path] = properties.message;
+				})
+	
+	return errors ;
+}
 
 // * POST un nuevo user
 const setUser = (req, res) => {
-	// * Como hash se ejecuta de forma asincrona debemos pasarle nuestro password dentro de ella
-	hashPassword(req.body.password)
-		.then(password => {
-			const {firstName,lastName,email} = req.body;
+	const {firstName, lastName, email, password} = req.body;
 
-			user.create({firstName, lastName, email, password})
-				.then(user => res.json(user))
-				.catch(err => res.status(400).json(err))
+	user.create({firstName, lastName, email,password})
+		.then(
+			user => {
+				res.status(201).json(user)
 		})
-		.catch(err => res.json(err))
+		.catch(
+			err => {
+				const errors = handleErrros(err);
+				res.status(400).json(errors)
+		})
 }
 
 module.exports = setUser;
