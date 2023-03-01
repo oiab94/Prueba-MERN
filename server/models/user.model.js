d3b4ec61355d818723c30e6b59f9e71c53f60cce
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-01 17:54:42 -0300

    +server: Hashea password antes de guardar en la DB

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
 	email:{
@@ -14,6 +15,16 @@ const userSchema = new mongoose.Schema({
 		minlength: [6, "Password length must be 6"]
 	}
 });
+
+// * Disparar una función antes de guardar un documento
+userSchema.pre("save", async function (next) {
+	// ! Este será el nuevo password hasheado
+	const salt = await bcrypt.genSalt(10);
+	
+	this.password = await bcrypt.hash(this.password, salt)
+
+	next();
+})
 
 const User = mongoose.model("user", userSchema);
 
