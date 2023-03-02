711867cd6a564f42a92cf1d5a4e4da1ebfbdaa55
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-02 12:34:16 -0300

    +client: Maneja los datos enviados al server

--- client/src/app/view/RegisterForm.jsx
+++ client/src/app/view/RegisterForm.jsx
@@ -2,14 +2,24 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import axios from "axios";
+import { useState } from "react";
 
 const url = "http://localhost:8000/api/signup";
 
 function RegisterForm() {
+	const [emailError, setEmailError] = useState({isInvalid: false, message: ""});
+	const [passwordError, setPasswordError] = useState({
+		isInvalid: false, message: ""})
+
+	// * Manejador de Submit
 	const handlerSubmit = (e) => {
 		const email = e.target["email"].value;
 		const password = e.target["password"].value;
 		e.preventDefault();
+
+		// Reset password
+		setEmailError({isInvalid: false, message: "", isValid: true});
+		setPasswordError({isInvalid: false, message: "", isValid: true});
 
 		// Envia los datos a la API
 		axios.post(url, { 
@@ -19,23 +29,46 @@ function RegisterForm() {
 			console.log(response);
 		})
 		.catch(function (error) {
-			console.log(error);
+			const {email, password} = error.response.data;
+			if(email !== "") 
+				setEmailError({message: email, isInvalid: true});
+			
+			if(password !== "")
+			setPasswordError({message: password, isInvalid: true});
 		})
 	}
 
 	return (
 		<Container>
-		<Form onSubmit={handlerSubmit}>
+		<Form noValidate={false} onSubmit={handlerSubmit}>
 			<Form.Label><h1>Sign Up</h1></Form.Label>
 			<Form.Group className="mb-3">
 				<Form.Label>Email address</Form.Label>
-				<Form.Control name="email" type="email" placeholder="Enter email" />
+				<Form.Control 
+					name="email" 
+					type="text" 
+					placeholder="Enter email" 
+					isValid={emailError.isValid}
+					isInvalid={emailError.isInvalid} />
+				<Form.Control.Feedback type="invalid">
+					{ emailError.message }
+				</Form.Control.Feedback>
 			</Form.Group>
 
 			<Form.Group className="mb-3">
 				<Form.Label>Password</Form.Label>
-				<Form.Control name="password" type="password" placeholder="Password" />
+				<Form.Control 
+					name="password" 
+					type="password" 
+					placeholder="Password"
+					isValid={ passwordError.isValid }
+					isInvalid={passwordError.isInvalid} />
+				<Form.Control.Feedback type="invalid">
+					{ passwordError.message }
+				</Form.Control.Feedback>
 			</Form.Group>
+
+
 			<Button variant="primary" type="submit">
 				Register
 			</Button>
