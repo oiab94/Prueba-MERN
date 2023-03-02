c46ac2adc8f802b0417da2a9667c87e474115240
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-03-01 21:51:38 -0300

    +client: Creación de un cliente con Login/Register form

---
+++ client/src/app/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoginForm from "./view/LoginForm";
+import RegisterForm from "./view/RegisterForm";
+
+function App() {
+  return (
+    <>
+      <RegisterForm />
+      <LoginForm />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/app/view/RegisterForm.jsx
@@ -0,0 +1,38 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container"
+
+function RegisterForm() {
+	const handlerSubmit = (e) => {
+		const email = e.target["email"];
+		const password = e.target["password"];
+		e.preventDefault();
+
+
+		// Obtener los valores
+		console.log(email.value);
+		console.log(password.value);
+	}
+
+	return (
+		<Container>
+		<Form onSubmit={handlerSubmit}>
+			<Form.Label><h1>Sign Up</h1></Form.Label>
+			<Form.Group className="mb-3">
+				<Form.Label>Email address</Form.Label>
+				<Form.Control name="email" type="email" placeholder="Enter email" />
+			</Form.Group>
+
+			<Form.Group className="mb-3">
+				<Form.Label>Password</Form.Label>
+				<Form.Control name="password" type="password" placeholder="Password" />
+			</Form.Group>
+			<Button variant="primary" type="submit">
+				Register
+			</Button>
+		</Form>
+		</Container>
+	);
+}
+
+export default RegisterForm;
